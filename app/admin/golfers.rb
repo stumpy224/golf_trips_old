64eb84c56cba74344deb57f70c6b5d1e409223c6
Jeremiah Stump <jstump@ncaa.org>
2018-12-10 14:23:37 -0500

    create Active Admin Users resource, only exposing the is_admin field

--- app/admin/golfers.rb
+++ app/admin/golfers.rb
@@ -20,22 +20,9 @@ ActiveAdmin.register Golfer do
       f.input :nickname
       f.input :email
       f.input :phone
-      f.input :is_active, label: 'Active?'
+      f.input :is_active, label: "Active?"
     end
     f.actions
   end
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
 end
