3715c371d508d2564c2047c3bb9dc3d9116f7997
Jeremiah Stump <jstump@ncaa.org>
2019-01-18 10:55:00 -0500

    show Expand All and Print button per outing day -- improvements still needed

--- app/assets/javascripts/outings.js
+++ app/assets/javascripts/outings.js
@@ -8,14 +8,16 @@ var columns = {
 };
 
 var selectors = {
-    collapse_icon: "fa-angle-down",
-    expansion_btn: "a.expansion-button",
-    expansion_icon: "i.fas",
-    expand_icon: "fa-angle-right"
+    btn_expand_all: "button.expand-all",
+    btn_expansion: "a.expansion-button",
+    icon_collapse: "fa-angle-down",
+    icon: "i.fas",
+    icon_expand: "fa-angle-right"
 };
 
 $(document).on('turbolinks:load', function() {
     setupTables();
+    setupExpandAllBtn();
     setupExpansionGolferTables();
 });
 
@@ -46,14 +48,26 @@ function setupTables() {
     });
 }
 
-function setupExpansionGolferTables() {
-    $(selectors.expansion_btn).click(function () {
-        var expansionElement = $(this).find(selectors.expansion_icon)[0];
+function setupExpandAllBtn() {
+    $(selectors.btn_expand_all).click(function () {
+        var btnText = $(this)[0].innerText.indexOf('EXPAND') > -1 ? "COLLAPSE ALL" : "EXPAND ALL";
+        setExpandCollapseIcon($(this).find(selectors.icon)[0]);
+        $(this)[0].innerHTML = $(this).find(selectors.icon)[0].outerHTML + btnText;
 
-        if ($(expansionElement).hasClass(selectors.expand_icon))
-            $(expansionElement).removeClass(selectors.expand_icon).addClass(selectors.collapse_icon);
-        else
-            $(expansionElement).addClass(selectors.expand_icon).removeClass(selectors.collapse_icon);
+        var tabId = $(this).closest('div.tab-pane')[0].id;
+        var outingDate = tabId.substring(tabId.indexOf("tab-for-") + "tab-for-".length, tabId.length);
+
+        $(selectors.btn_expand_all).closest('div.row').next('table').find('tr.team-row').each(function () {
+            // find all anchors whose href ends with the outingDate, and click them to expand / collapse
+            $(this).find('[href$=' + outingDate + ']').click();
+        });
+    });
+}
+
+function setupExpansionGolferTables() {
+    $(selectors.btn_expansion).click(function () {
+        var expansionElement = $(this).find(selectors.icon)[0];
+        setExpandCollapseIcon(expansionElement);
     });
 
     $("a.dropdown-item[href^='#tab-for-']").click(function () {
@@ -62,9 +76,16 @@ function setupExpansionGolferTables() {
         $("tr.golfer-scores-table-collapse").removeClass("show");
 
         // reset the expand/collapse icons
-        $(selectors.expansion_btn).each(function () {
-            var expansionElement = $(this).find(selectors.expansion_icon)[0];
-            $(expansionElement).addClass(selectors.expand_icon).removeClass(selectors.collapse_icon);
+        $(selectors.btn_expansion).each(function () {
+            var expansionElement = $(this).find(selectors.icon)[0];
+            $(expansionElement).addClass(selectors.icon_expand).removeClass(selectors.icon_collapse);
         });
     })
+}
+
+function setExpandCollapseIcon(expansionElement) {
+    if ($(expansionElement).hasClass(selectors.icon_expand))
+        $(expansionElement).removeClass(selectors.icon_expand).addClass(selectors.icon_collapse);
+    else
+        $(expansionElement).addClass(selectors.icon_expand).removeClass(selectors.icon_collapse);
 }
